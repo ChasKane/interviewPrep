031f38d6e007f39f5e49a9b36c1c26071c0d7819
ChasKane <LawrenceCKane@gmail.com>
2018-08-18 17:27:22 -0400

    questions 2 and 3 done

--- stringQuestions/string_questions.py
+++ stringQuestions/string_questions.py
@@ -1,11 +1,51 @@
+# I've omitted input validation since
+# these would all have similar validation requirements.
+# comments on runtimes are worst case unless specified.
 def print_dups(s):
+    # wonder if there's a way to do this without `seen`...
+    # not super important, as runtime is O(n^2): n=len(s)
+    # for x in s:
+    #   seen = False
+    #   for y in s:
+    #       if x == y:
+    #           if seen: print(y)
+    #           else: seen = True;
+    #
+    # below runtime is O(1), since len(alpha)<=26
     result = ""
-    if not s: return result
     alpha = ""
     for c in s:
         if c in alpha:
             result += c
         else:
             alpha += c
-
     return result
+
+def is_anagram(s1, s2):
+    # lol potentailly infinite runtime if shuffle() is random
+    # while True:
+    #   if s1.shuffle() == s2: return True
+
+    # heh, even better, as it's one line and clever, but
+    # we must assume (due to sorting) that it's O(n log n): n=max(len(s1), len(s2))
+    # return sorted(s1) == sorted(s2)
+
+    # below runtime is O(n): n=max(len(s1), len(s2))
+    if len(s1) != len(s2): return False
+    letterCounts1 = {}
+    letterCounts2 = {}
+    for c1, c2 in zip(s1, s2):
+        if c1 in letterCounts1: letterCounts1[c1] += 1
+        else: letterCounts1[c1] = 1
+        if c2 in letterCounts2: letterCounts2[c2] += 1
+        else: letterCounts2[c2] = 1
+    return letterCounts1 == letterCounts2
+
+def first_nonrepeated_character(s):
+    letterCounts = {}
+    for c in s:
+        if c in letterCounts: letterCounts[c] += 1
+        else: letterCounts[c] = 1
+    for c in s:
+        if letterCounts[c] == 1: return c
+    return ""
--- stringQuestions/test_string_questions.py
+++ stringQuestions/test_string_questions.py
@@ -2,13 +2,19 @@ from string_questions import *
 import unittest
 
 class test_print_dups(unittest.TestCase):
-    def test_empty_string(self):
-        self.assertEqual(print_dups(""), "")
     def test_a_few_strings(self):
         self.assertEqual(print_dups("aa"), "a")
         self.assertEqual(print_dups("abcdefghijklmnop"), "")
         self.assertEqual(print_dups("abcdefghijklmnopaa"), "aa")
+class test_is_anagram(unittest.TestCase):
+    def test_a_few_strings(self):
+        self.assertEqual(is_anagram("aa", ""), False)
+        self.assertEqual(is_anagram("abcd", "badc"), True)
+class test_first_nonrepeated_character(unittest.TestCase):
+    def test_a_few_strings(self):
+        self.assertEqual(first_nonrepeated_character("aa"), "")
+        self.assertEqual(first_nonrepeated_character("aabcdefghijklmnopbd"), "c")
+        self.assertEqual(first_nonrepeated_character("abcdabcdabcd"), "")
 
 if __name__ == '__main__':
     unittest.main()
-
