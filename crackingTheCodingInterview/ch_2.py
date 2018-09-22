4afcabbc3c4cdadb9e496043cc7f77cbc51fb458
ChasKane <LawrenceCKane@gmail.com>
2018-09-22 13:28:51 -0400

    Spent 3 hours reading about python namespacing and classes... no codelol

---
+++ crackingTheCodingInterview/ch_2.py
@@ -0,0 +1,13 @@
+# Cusom singly linked list implementation, checked internet afterward to ensure
+# no bugs -- any updates at this point are noted in comments
+class Node():
+
+class LinkedList():
+    
+# 2.1
+# Write code to remove duplicates from an unsorted linked list.
+# How would you solve this problem if a temporary buffer is not allowed?
+# INPUT: unsorted LL of lowercase letters, "any" length
+# OUTPUT: the same LL object without duplicate nodes
+def remove_dups(LL):
+    return LL
---
+++ crackingTheCodingInterview/test_ch_2.py
@@ -0,0 +1,9 @@
+from ch_2 import *
+import unittest
+
+class test_ch_2(unittest.TestCase):
+    def test_remove_dups(self):
+        self.assertEqual(remove_dups("aa"), False)
+
+if __name__ == '__main__':
+    unittest.main()
